5ebfe686c26c02c995650306218f2f58a83cbcea
Jay <jekelle@iu.edu>
2026-02-25 16:23:27 -0500

    Remove docstrings from Genius API functions
    Removed docstrings from genius, genius_to_df, and genius_to_dfs functions to simplify the code.

--- genius_api.py
+++ genius_api.py
@@ -1,10 +1,9 @@
-# built-in
+
 import requests
 import os
 from multiprocessing import Pool
 from time import sleep
 
-# user-installed
 import pandas as pd
 from tqdm import tqdm
 from numpy.random import uniform
@@ -12,28 +11,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# constants
+
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 NAME_DEMO = __name__
 
 def genius(search_term, per_page=15):
-    """
-    Collect data from the Genius API by searching for `search_term`.
-    
-    **Assumes ACCESS_TOKEN is loaded in environment.**
-
-    Parameters
-    ----------
-    search_term : str
-        The name of an artist, album, etc.
-    per_page : int, optional
-        Maximum number of results to return, by default 15
-
-    Returns
-    -------
-    list
-        All the hits which match the search criteria.
-    """
+ 
     genius_search_url = f"http://api.genius.com/search?q={search_term}&" + \
                         f"access_token={ACCESS_TOKEN}&per_page={per_page}"
     
@@ -44,26 +27,12 @@ def genius(search_term, per_page=15):
 
 def genius_to_df(search_term, n_results_per_term=10, 
                  verbose=True, savepath=None):
-    """
-    Generate a pandas.DataFrame from a single call to the Genius API.
-
-    Parameters
-    ----------
-    search_term : str
-        Genius search term
-    n_results_per_term : int, optional
-        Number of results "per_page" for each search term provided, by default 10
-
-    Returns
-    -------
-    pandas.DataFrame
-        The final DataFrame containing the results. 
-    """
+ 
     json_data = genius(search_term, per_page=n_results_per_term)
     hits = [hit['result'] for hit in json_data]
     df = pd.DataFrame(hits)
 
-    # expand dictionary elements
+
     df_stats = df['stats'].apply(pd.Series)
     df_stats.rename(columns={c:'stat_' + c for c in df_stats.columns},
                     inplace=True)
@@ -79,7 +48,6 @@ def genius_to_df(search_term, n_results_per_term=10,
         print(f'PID: {os.getpid()} ... search_term:', search_term)
         print(f"Data gathered for {search_term}.")
 
-    # this is a good practice for numerous automated data pulls ...
     if savepath:
         df.to_csv(savepath + '/genius-{searchname}.csv', 
                   index=False)
@@ -87,29 +55,15 @@ def genius_to_df(search_term, n_results_per_term=10,
     return df
 
 def genius_to_dfs(search_terms, **kwargs):
-    """
-    Generate a pandas.DataFrame from multiple calls to the Genius API.
 
-    Parameters
-    ----------
-    search_terms : list of strings
-        List of artists (say) to search in Genius.
-    
-    **kwargs : arguments passed to genius_api.genius_to_df
-
-    Returns
-    -------
-    pandas.DataFrame
-        The final DataFrame containing the results. 
-    """
 
     dfs = []
 
-    # loop through search_terms in question
+ 
     for search_term in tqdm(search_terms):
         df = genius_to_df(search_term, **kwargs)
         
-        # add to list of DataFrames
+   
         dfs.append(df)
 
     return pd.concat(dfs)
@@ -124,7 +78,6 @@ def job_test(num, mult=2):
     return num * mult
 
 
-# print("__name__ is", __name__)
 if __name__ == "__main__":
 
   
